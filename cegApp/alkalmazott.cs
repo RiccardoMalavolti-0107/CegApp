d8d7548ab89d106c47d5daf7e3a668fc1a242114
RiccardoMalavolti-010 <malavolti.riccardo.2022d@telekimezotur.hu>
2026-09-24 09:39:38 +0200

    asd

---
+++ cegApp/alkalmazott.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cegApp
+{
+    internal class alkalmazott
+    {
+        public string nev { get; set; }
+
+        protected int Alapber { get; set; }
+
+        public alkalmazott(string nev, int alapber)
+        {
+            this.nev = nev;
+            this.Alapber = alapber;
+        }
+
+        public virtual int fizetesSzamitas()
+        {
+            return Alapber;
+        }
+        public override string ToString()
+        {
+            return $"Név: {nev}, Alapbér: {Alapber}";
+        }
+
+    }
+}
